6ba97a9bc382b9a0946e78fd0c6850e8bd5c1c3c
Samyak Agrawal <samyakagrawal13@gmail.com>
2022-06-01 13:15:45 +0530

    Added address

--- models/Users.js
+++ models/Users.js
@@ -1,23 +1,20 @@
 const mongoose = require("mongoose");
 
 const UsersSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
+	name: {type: String, required: true},
 	email: {type: String, required: true, unique: true},
-	address: {
-		type: String,
-		required: false,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	imageUri: {
-		type: String,
-		required: false,
-	},
+	address: {type: String, required: false},
+	password: {type: String, required: true},
+	imageUri: {type: String, required: false},
+	address: {type: AddressSchema, required: false},
+});
+
+const AddressSchema = new mongoose.Schema({
+	houseNo: {type: String, required: true},
+	street: {type: String, required: true},
+	city: {type: String, required: true},
+	state: {type: String, required: true},
+	pincode: {type: String, required: true},
 });
 
 const model = mongoose.model("Users", UsersSchema);
